55088b90ec09ce0e019891b3af7fdbb61187521b
azmidri <azmidri@yahoo.com>
2022-03-15 16:46:16 +0300

    stats

--- Beautifly_B/stats_bs.py
+++ Beautifly_B/stats_bs.py
@@ -6,6 +6,6 @@ class Stats_BS():
     def __init__(self):
         super().__init__()  
     def make_stats(n):
-        return stats.kurtosis(n), stats.gmean(n),stats.skew(n)
+        return stats.kurtosis(n, nan_policy = 'omit'), stats.gmean(n),stats.skew(n, nan_policy = 'omit')
 
  
